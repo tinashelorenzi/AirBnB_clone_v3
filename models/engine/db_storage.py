0edc4364e7ff3d66e9f64d4082bc1d5f973a0be3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-28 13:31:21 +0100

    Added get and count methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -82,3 +82,17 @@ class DBStorage:
         """ calls remove()
         """
         self.__session.close()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        classes = self.all(cls)
+        for key, value in classes.items():
+            if key.split('.')[1] == id:
+                return (value)
+        return (None)
+
+    def count(self, cls=None):
+        """Counts the number of objects in storage"""
+        if (cls):
+            return (len(self.all(cls)))
+        return (len(self.all()))
