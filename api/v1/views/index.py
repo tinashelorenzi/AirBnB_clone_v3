7be4f4333fdba8c50620f38f61925b40ae27668b
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-29 12:39:08 +0100

    Resolved pycodestyle error

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,9 +11,9 @@ from models.state import State
 from models.user import User
 
 
-classes = { "amenities": Amenity, "cities": City, "places": Place,
+classes = {"amenities": Amenity, "cities": City, "places": Place,
            "reviews": Review, "states": State, "users": User
-        }
+           }
 
 
 @app_views.route("/status")
