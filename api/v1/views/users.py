924bde741f230931a665e7e6944bf2d3b05c5c61
Tinashe <tinashelorenzi@protonmail.com>
2023-07-29 22:14:35 +0200

    Removed flask_doc

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,11 +4,9 @@ from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/all_users.yml')
 def get_users():
     """
     Retrieves the list of all user objects
@@ -22,9 +20,8 @@ def get_users():
 
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/user/get_user.yml', methods=['GET'])
 def get_user(user_id):
-    """ Retrieves an user """
+    """ Retrieves an user based on id """
     user = storage.get(User, user_id)
     if not user:
         abort(404)
@@ -34,10 +31,9 @@ def get_user(user_id):
 
 @app_views.route('/users/<user_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/user/delete_user.yml', methods=['DELETE'])
 def delete_user(user_id):
     """
-    Deletes a user Object
+    Deletes a user Obj
     """
 
     user = storage.get(User, user_id)
@@ -52,7 +48,6 @@ def delete_user(user_id):
 
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-@swag_from('documentation/user/post_user.yml', methods=['POST'])
 def post_user():
     """
     Creates a user
@@ -72,10 +67,9 @@ def post_user():
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/user/put_user.yml', methods=['PUT'])
 def put_user(user_id):
     """
-    Updates a user
+    Updates a user based on given id
     """
     user = storage.get(User, user_id)
 
