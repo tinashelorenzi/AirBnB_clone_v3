9436aa37c6e04ac8dc8b4439903b681ff49e4ef3
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-29 10:14:42 +0100

    Handles 404 errors and request made to /stats

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
-"""Flask web server application"""
+"""Initialization for the flask application are done here"""
+from flask import Flask, jsonify, abort
 from models import storage
+from api.v1.views import app_views
+from os import getenv
+
 
 app = Flask(__name__)
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Close Storage Module """
+def teardown(arg):
+    """Removes the current Session"""
     storage.close()
 
+
 @app.errorhandler(404)
-def not_found(error):
-    """ 404 Error
-    ---
-    responses:
-      404:dir
-        description: a resource was not found
-    """
-    return make_response(jsonify({'error': "Not found"}), 404)
+def page_not_found(error):
+    """Handles request to unhandled paths"""
+    return (jsonify({ "error": "Not found" }), 404)
+
 
 if __name__ == "__main__":
-    """ Main Function """
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
+    host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
+    port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
     app.run(host=host, port=port, threaded=True)
