5f770b50630f6f9fbdf426f9bda290bb91b2b937
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-07-29 17:18:12 +0000

    Change app views call

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 from flask import Blueprint
-from models.state import State
 """Initializes the Blueprint"""
 
 
-app_views = Blueprint("/api/v1", __name__)
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
 from api.v1.views.index import *
