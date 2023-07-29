ad84656fdc4f71865c1f971fa5a89ff3b1acc60e
mcdonaldcm7 <mcdonald.cm7@gmail.com>
2023-07-29 12:01:25 +0100

    Contains definition for the function that handles '/status' route

---
+++ api/v1/views/index.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module for function that defines the handles for incoming requests"""
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = { "amenities": Amenity, "cities": City, "places": Place,
+           "reviews": Review, "states": State, "users": User
+        }
+
+
+@app_views.route("/status")
+def status():
+    """Handles status request"""
+    return (jsonify({"status": "OK"}))
+
+
+@app_views.route("/stats")
+def stats():
+    """Retrieves the number of each object by type"""
+    stats = {}
+    for key, value in classes.items():
+        stats[key] = storage.count(value)
+    print("In stats function: \nstats is {}".format(stats))
+    return (jsonify(stats), 404)
