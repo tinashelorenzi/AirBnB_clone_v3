fb9bbaaf3872cd9a625b0700ee99d1e98593cdbb
Tinashe <tinashelorenzi@protonmail.com>
2023-07-29 22:08:47 +0200

    Minor fixes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-"""
-Module for function that defines the handles for incoming requests to
-different routes
-"""
-from flask import jsonify
-from models import storage
+""" Index """
 from models.amenity import Amenity
 from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
+from models import storage
 from api.v1.views import app_views
+from flask import jsonify
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """
-    Handles status request by returning a JSON with 'status' value OK
-    """
+    """ Status of API """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def count():
-    """Retrieves the number of each object by type"""
+def number_objects():
+    """ Retrieves the number of each objects by type """
     classes = [Amenity, City, Place, Review, State, User]
     names = ["amenities", "cities", "places", "reviews", "states", "users"]
 
