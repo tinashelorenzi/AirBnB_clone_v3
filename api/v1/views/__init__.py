7623ca3c66179f1d06e12191405282ebf467759b
Tinashe Matanda <117823479+tinashelorenzi@users.noreply.github.com>
2023-07-29 18:31:15 +0000

    Code fixes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """BluePrint to the api pieces"""
 from flask import Blueprint
-"""Initializes the Blueprint"""
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
